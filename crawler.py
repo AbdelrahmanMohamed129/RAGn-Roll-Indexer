04be5654af8a52e2c99997a77acc7c892a64e0c5
farah-moh <75073858+farah-moh@users.noreply.github.com>
2024-04-24 00:04:48 +0200

    Integrated TopK Query Retriever

--- crawler.py
+++ crawler.py
@@ -1,44 +1,74 @@
 import grequests
 from lxml import html
 import time
+import json
 
-# read the top k pages from file "top_k.txt"
-with open("top_k_ids.txt", "r") as f:
-    pages = f.readlines()
+# # read the top k pages from file "top_k.txt"
+# with open("top_k_ids.txt", "r") as f:
+#     pages = f.readlines()
 
-# generate urls for each page
-urls = dict()
-for page in pages:
-    actual_page = int(page)//100
-    row = int(page)%100 + 1
-    url = "https://huggingface.co/datasets/Cohere/wikipedia-22-12-simple-embeddings/viewer/default/train?p=" + str(actual_page)
-    if url not in urls:
-        urls[url] = list()
-    urls[url].append(row)
+# # generate urls for each page
+# urls = dict()
+# for page in pages:
+#     actual_page = int(page)//100
+#     row = int(page)%100 + 1
+#     url = "https://huggingface.co/datasets/Cohere/wikipedia-22-12-simple-embeddings/viewer/default/train?p=" + str(actual_page)
+#     if url not in urls:
+#         urls[url] = list()
+#     urls[url].append(row)
 
-start = time.time()
+# start = time.time()
 
 
-# send requests to urls
-rs = (grequests.get(u) for u in urls)
-responses = grequests.map(rs)
+# # send requests to urls
+# rs = (grequests.get(u) for u in urls)
+# responses = grequests.map(rs)
 
-top_k_data = list()
+# top_k_data = list()
 
-# parse the response and get the top k data [text]
-for response in responses:
-    content = response.text
-    parsed = html.fromstring(content)
-    # get the rows in urls[response.url]
-    rows = parsed.xpath("/html/body/div[1]/main/div/div/div/div[2]/div/table/tbody/tr")
-    for row in urls[response.url]:
-        text = rows[row-1].xpath("td[3]/div/div/text()")[0]
-        top_k_data.append(text)
+# # parse the response and get the top k data [text]
+# for response in responses:
+#     content = response.text
+#     parsed = html.fromstring(content)
+#     # get the rows in urls[response.url]
+#     rows = parsed.xpath("/html/body/div[1]/main/div/div/div/div[2]/div/table/tbody/tr")
+#     for row in urls[response.url]:
+#         text = rows[row-1].xpath("td[3]/div/div/text()")[0]
+#         top_k_data.append(text)
 
-end = time.time()
-print("Time taken to crawl the top K: ",end - start) #time taken to run the code
+# end = time.time()
+# print("Time taken to crawl the top K: ",end - start) #time taken to run the code
 
-# write the data to file
-with open("top_k_data.txt", "w") as f:
-    for data in top_k_data:
-        f.write(data + "\n")
+# # write the data to file
+# with open("top_k_data.txt", "w") as f:
+#     for data in top_k_data:
+#         f.write(data + "\n")
+
+# import requests
+# headers = {"Authorization": f"Bearer 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"}
+# API_URL = "https://datasets-server.huggingface.co/rows?dataset=wiki_dpr&config=psgs_w100.multiset.exact&split=train&offset=9000000&length=1"
+# def query():
+#     response = requests.get(API_URL, headers=headers)
+#     return response.json()
+# data = query()
+# # with open("nashaat.txt","w") as f:
+# #     json.dump(data, f)
+# print(json.dumps(data, indent=4))
+
+import pandas as pd
+from datasets import load_dataset
+from huggingface_hub import HfFileSystem
+token = "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"
+fs = HfFileSystem(token=token)
+print("authenticated")
+fs.ls("datasets/wiki_dpr/blob")
+
+# start = time.time()
+# # load the dataset
+# base_url = "https://huggingface.co/datasets/wiki_dpr/blob/main/data/psgs_w100/nq/train-00000-of-00157.parquet"
+# data_files = {"train": base_url }
+# dataset = load_dataset("parquet", data_files=data_files, split="train")
+# end = time.time()
+# print("Time taken to read the file: ",end - start) #time taken to run the code
+# # print the row whose id is 10
+# print(dataset[10])
--- queryEncoder.py
+++ queryEncoder.py
@@ -1,13 +1,12 @@
 import cohere
 import os
-import pandas as pd
+import faiss 
+import numpy as np
 
 os.environ['COHERE_API_KEY'] = 'gtNwvMCXjn0HnBpZ42YEbMMoXz6BqDvXY2aoQRoM'
 co = cohere.Client(os.environ['COHERE_API_KEY'])
 
-texts = [
-   'When was Israeli Prime Minister Yitzhak Rabin assassinated'
-]
+texts = ['When was Israeli Prime Minister Yitzhak Rabin assassinated']
 response = co.embed(texts=texts, model='multilingual-22-12')
 embeddings = response.embeddings # All embeddings for the texts
 print(embeddings[0][:5]) # Let's check embeddings for the first text
@@ -15,3 +14,30 @@ print(embeddings[0][:5]) # Let's check embeddings for the first text
 with open('./Data/query.txt', 'w') as f:
     f.write(str(embeddings).replace('[','').replace(']','').replace(',',''))
     f.close()
+
+# read queries and compute nearest cloisters
+np.set_printoptions(threshold=np.inf, linewidth=np.inf)  # ensure numpy array won't truncate
+
+# read the faiss index
+index = faiss.read_index("./Data/KMeansFAISS/index.bin")
+
+with open("./Data/query.txt", "r") as f:
+    lines = [np.fromstring(line, sep=" ") for line in f]
+    queries = np.array(lines)
+
+#Normalize the query
+queries = queries / np.linalg.norm(queries)
+
+_, I = index.search(queries, 10)
+I_flat = [i for i in I]
+
+# write the labels to a file where I_flat is the centroid index for each embedding
+with open("./Data/labels/query_faiss_labels.txt", "a") as f:
+    f.write(str(I_flat).replace("[", "").replace("]", "").replace(",", "").replace("  ", " ").strip())
+
+headerFile = "D:/Boody/GP/Indexer/RAGn-Roll-Indexer/";
+# Arguments are topK (int), headerFile (string)
+topK = 10
+command = f"{headerFile}/cmake-build-debug/RAGn_Roll_Indexer.exe {topK} {headerFile} -lboost_serialization -lboost_system"
+print(command)
+os.system(command)
